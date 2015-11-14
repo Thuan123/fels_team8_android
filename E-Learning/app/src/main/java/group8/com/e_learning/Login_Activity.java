072878da3a6ea53cadee8d7667e94fba572997a9
linhtn <hotdog160993@gmail.com>
2015-11-14 14:42:06 +0700

    create login

---
+++ E-Learning/app/src/main/java/group8/com/e_learning/Login_Activity.java
@@ -0,0 +1,15 @@
+package group8.com.e_learning;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tranngoclinh on 11/14/15.
+ */
+public class Login_Activity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
