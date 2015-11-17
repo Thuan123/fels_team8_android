48eea3737aee98115a50aae0d8e809a1b4109464
linhtn <hotdog160993@gmail.com>
2015-11-17 23:32:15 +0700

    scroll view

--- E-Learning/app/src/main/java/group8/com/e_learning/Profile_Activity.java
+++ E-Learning/app/src/main/java/group8/com/e_learning/Profile_Activity.java
@@ -1,15 +1,35 @@
 package group8.com.e_learning;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by tranngoclinh on 11/15/15.
  */
-public class Profile_Activity extends Activity {
+public class Profile_Activity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_edit:
+                Intent intentEdit = new Intent(Profile_Activity.this, UpdateProfile_Activity.class);
+                startActivity(intentEdit);
+                break;
+            case R.id.btn_words:
+                Intent intentWords = new Intent(Profile_Activity.this, WordList_Activity.class);
+                startActivity(intentWords);
+                break;
+            case R.id.btn_lesson:
+                Intent intentLesson = new Intent(Profile_Activity.this, Lesson_Basic_Activity.class);
+                startActivity(intentLesson);
+                break;
+        }
     }
 }
