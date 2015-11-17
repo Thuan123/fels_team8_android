7c18f43c71ab1d56eab7e97189dc9b58c233c3ab
namnx228 <namnx228@gmail.com>
2015-11-17 19:18:58 +0700

    revert after merge

--- E-Learning/app/src/main/java/group8/com/e_learning/database/SQLiteHelper.java
+++ E-Learning/app/src/main/java/group8/com/e_learning/database/SQLiteHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import group8.com.e_learning.common.Application;
 import group8.com.e_learning.database.ELearningDB.Elearning;
 /**
  * Created by nomore on 12/11/2015.
@@ -12,6 +13,13 @@ public class SQLiteHelper extends SQLiteOpenHelper{
 
     private static final String TEXT_TYPE = " TEXT";
     private static final String COMMA_SEP = ",";
+
+    private static final String SQL_CREATE_USER =
+                        "CREATE TABLE " + Elearning.TABLE_USER + " (" +
+                    Elearning._ID + " INTEGER PRIMARY KEY," +
+
+                                Elearning.COLUMN_CREATE_AT + TEXT_TYPE + COMMA_SEP +
+                                Elearning.COLUMN_UPDATE_AT + TEXT_TYPE +" )";
     /*private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + ELearningDB.Elearning.TABLE_NAME + " (" +
                     Elearning._ID + " INTEGER PRIMARY KEY," +
@@ -25,6 +33,13 @@ public class SQLiteHelper extends SQLiteOpenHelper{
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "ELearning.db";
+    private static SQLiteHelper instace;
+
+    public static SQLiteHelper getInstace()
+    {
+        if (instace == null) instace = new SQLiteHelper(Application.getInstance());
+        return instace;
+    }
 
     public SQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -109,7 +124,7 @@ public class SQLiteHelper extends SQLiteOpenHelper{
         */
 
 
-    }
+//    }
 
 
     public void deleteFromDb()
