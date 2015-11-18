871581ba07edd54a172b14cb13ac782cbcc97aa5
linhtn <hotdog160993@gmail.com>
2015-11-19 04:26:04 +0700

    complete Layout

--- E-Learning/app/src/main/java/group8/com/e_learning/Register_Activity.java
+++ E-Learning/app/src/main/java/group8/com/e_learning/Register_Activity.java
@@ -3,32 +3,30 @@ package group8.com.e_learning;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by tranngoclinh on 11/15/15.
  */
-public class Register_Activity extends Activity {
+public class Register_Activity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        initView();
     }
 
-    private void initView() {
-        TextView tvEmail, tvPassword, tvRePassword, tvFullName, tvRequied;
-        tvEmail = (TextView) findViewById(R.id.tv_email);
-        tvPassword = (TextView) findViewById(R.id.tv_pass);
-        tvRePassword = (TextView) findViewById(R.id.tv_retype_pass);
-        tvFullName = (TextView) findViewById(R.id.tv_full_name);
-        tvRequied = (TextView) findViewById(R.id.tv_required);
-        tvEmail.setText(Html.fromHtml("\tEmail <sup>*</sup>"));
-        tvPassword.setText(Html.fromHtml("\tPassword <sup>*</sup>"));
-        tvRePassword.setText(Html.fromHtml("\tRetype password <sup>*</sup>"));
-        tvFullName.setText(Html.fromHtml("\tFull name <sup>*</sup>"));
-        tvRequied.setText(Html.fromHtml("<sup>*</sup>Required information"));
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_cancel:
+                finish();
+                break;
+            case R.id.btn_done:
+                Toast.makeText(this, "Register done", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
