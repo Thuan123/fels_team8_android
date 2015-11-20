fd02117863c372584954d5fa295a2437465a8d0b
linhtn <hotdog160993@gmail.com>
2015-11-20 22:47:42 +0700

    word recycle

--- E-Learning/app/src/main/java/group8/com/e_learning/Profile_Activity.java
+++ E-Learning/app/src/main/java/group8/com/e_learning/Profile_Activity.java
@@ -3,12 +3,16 @@ package group8.com.e_learning;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by tranngoclinh on 11/15/15.
  */
 public class Profile_Activity extends Activity implements View.OnClickListener {
+    private TextView name,email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
