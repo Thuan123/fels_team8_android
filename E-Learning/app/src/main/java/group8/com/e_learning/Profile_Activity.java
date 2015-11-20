f547f5a40f663d47176249a208315011ba746f2e
linhtn <hotdog160993@gmail.com>
2015-11-20 20:46:05 +0700

    recycle view profile

--- E-Learning/app/src/main/java/group8/com/e_learning/Profile_Activity.java
+++ E-Learning/app/src/main/java/group8/com/e_learning/Profile_Activity.java
@@ -3,6 +3,8 @@ package group8.com.e_learning;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 /**
@@ -13,6 +15,16 @@ public class Profile_Activity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        setRecycleView();
+    }
+    private void setRecycleView(){
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rv_learned);
+        // Create adapter passing in the sample user data
+        ItemLearnedAdapter adapter = new ItemLearnedAdapter(ItemLearned.createItemLearned(20));
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
